ae18d322eaf9f9058ead69ee2e7b1ea87a0b5b5c
Gabriel Aguilar <gabonfc1650@gmail.com>
2024-11-19 14:51:33 -0500

    Model CreditCard

---
+++ src/main/java/com/parquesoftti/tc/model/CreditCard.java
@@ -0,0 +1,29 @@
+package com.parquesoftti.tc.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = CreditCard)
+public class CreditCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String cardNumber;
+
+    @Column(nullable = false)
+    private String expirtationDate;
+
+    @Column(nullable = false)
+    private String cvv;
+}
